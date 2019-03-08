94b9eeb9d694cc67888f92246474d2b5403f855f
leilinsu <779479877@qq.com>
2019-03-07 22:40:39 -0700

    added send sms function, and ask permission to main.

--- app/src/main/java/com/example/jacobgraves/myapplication/view/MainActivity.kt
+++ app/src/main/java/com/example/jacobgraves/myapplication/view/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.example.jacobgraves.myapplication.view
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.location.*
+import android.support.v4.app.ActivityCompat
 import android.util.Log
 import com.example.jacobgraves.myapplication.R
+import com.example.jacobgraves.myapplication.view.SMSUtility.SendSMS
 import kotlinx.android.synthetic.main.activity_main.*
 
 private var locationManager:LocationManager? = null
 
 class MainActivity : AppCompatActivity() {
-
-
+    private val requestSendSms: Int = 2
+    public var ifEnableSms: Boolean = false;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,7 +41,20 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.SEND_SMS), requestSendSms)
+        }
+
+
+
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if(requestCode == requestSendSms) ifEnableSms = true;
+
+    }
+
+
 
     private val locationListener:LocationListener = object:LocationListener {
 
