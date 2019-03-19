d9e20b2d31a6549fbc993daaee18d1caf30e40c4
Mohamed Toualbi <mohamedtoualbi@SUU.EDU>
2019-03-19 14:00:57 -0600

    SMSUtils + newRaven Data Retrieval

--- app/src/main/java/com/example/jacobgraves/myapplication/view/NewRaven.kt
+++ app/src/main/java/com/example/jacobgraves/myapplication/view/NewRaven.kt
@@ -1,13 +1,50 @@
 package com.example.jacobgraves.myapplication.view
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.jacobgraves.myapplication.R
+import com.example.jacobgraves.myapplication.view.model.Raven
+import kotlinx.android.synthetic.main.activity_new_raven.*
 
 class NewRaven : AppCompatActivity() {
+
+    var ravenName: String = ""
+    var ravenPhoneNo: String = ""
+    var ravenAddress: String = ""
+    var ravenLongitude: Double = 0.0
+    var ravenLatitude: Double = 0.0
+    var ravenMessage: String = ""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_raven)
+
+        val goBackToMainActivity: Intent = Intent(applicationContext, MainActivity::class.java)
+
+        validButton.setOnClickListener {
+
+            ravenName = enteredName.text.toString()
+            ravenPhoneNo = enteredNumber.text.toString()
+            ravenAddress = enteredAddress.text.toString()
+            ravenMessage = enteredMessage.text.toString()
+
+            //TODO - REVERSE GEOCODING TO GET LONG & LAT FROM ADDRESS.
+
+            val raven: Raven = Raven(ravenName, ravenPhoneNo, ravenMessage, ravenLongitude, ravenLatitude)
+
+            //TODO - SAVE RAVEN INTO DATABASE.
+
+            startActivity(goBackToMainActivity)
+
+        }
+
+        cancelButton.setOnClickListener {
+
+            startActivity(goBackToMainActivity)
+
+        }
+
     }
 }
--- app/src/main/java/com/example/jacobgraves/myapplication/view/model/Raven.kt
+++ app/src/main/java/com/example/jacobgraves/myapplication/view/model/Raven.kt
@@ -1,4 +1,4 @@
-package model
+package com.example.jacobgraves.myapplication.view.model
 
 class Raven(var name: String, var phoneNo: String, var message: String, var longitude: Double, var latitude: Double) {
 
