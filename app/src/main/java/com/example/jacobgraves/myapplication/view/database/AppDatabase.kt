55d46e5be2ec84851d6d83a2c8cb331f57f95c50
AmineToualbi <amtoualbi@gmail.com>
2019-03-20 10:55:44 -0600

    Start of SQLite Implementation using Dagger & Room.

--- app/src/main/java/com/example/jacobgraves/myapplication/view/database/AppDatabase.kt
+++ app/src/main/java/com/example/jacobgraves/myapplication/view/database/AppDatabase.kt
@@ -1,4 +1,20 @@
 package com.example.jacobgraves.myapplication.view.database
 
-class AppDatabase {
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.RoomDatabase
+import com.example.jacobgraves.myapplication.view.database.daos.RavenDao
+import com.example.jacobgraves.myapplication.view.model.Raven
+
+@Database(
+        entities = [
+        Raven::class
+        ],
+        version = 2,
+        exportSchema = false
+)
+
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun ravenDao(): RavenDao
+
 }
