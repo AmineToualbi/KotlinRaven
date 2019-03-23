02cf78d85f4bec9cd126bb29ced99a508b226db2
mallekr <33009761+mallekr@users.noreply.github.com>
2019-03-22 22:34:28 -0600

    Working on permissions. Not fully implemented. Work in progress, do not pull please.

--- app/src/main/java/com/example/jacobgraves/myapplication/view/MainActivity.kt
+++ app/src/main/java/com/example/jacobgraves/myapplication/view/MainActivity.kt
@@ -15,7 +15,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.Toast
 import android.widget.CompoundButton
 import android.widget.Switch
-
+import com.example.jacobgraves.myapplication.view.permissions.requestPermission
 
 
 private var locationManager:LocationManager? = null
@@ -23,6 +23,8 @@ private var locationManager:LocationManager? = null
 class MainActivity : AppCompatActivity() {
     private val requestSendSms: Int = 2
     public var ifEnableSms: Boolean = false;
+    private val PermissionRequestCode = 23
+    private lateinit var req_permission: requestPermission
 
     companion object {          //Equivalent of public static var.
         var ravenID: Int = 0
@@ -32,10 +34,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val permissionList = listOf<String>(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.SEND_SMS
+        )
+
+        req_permission = requestPermission(this,permissionList,PermissionRequestCode)
+
         //Persistent LocationManager reference
         locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager?
 
         try {
+            req_permission.checkPermissions()
             locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000L, 10f, locationListener)
         }catch (ex: SecurityException){
             Log.d("myTag", "Security Exception, no location available")
@@ -62,18 +73,18 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+        /*if (ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.SEND_SMS), requestSendSms)
-        }
+        }*/
 
 
 
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    /*override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         if(requestCode == requestSendSms) ifEnableSms = true;
 
-    }
+    }*/
 
 
 
